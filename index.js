c9c07af827d2fa0cd6f1f2b61c035b3a0aacf697
shadmansaalim321 <shadmansaalim321@gmail.com>
2022-04-23 03:33:20 +1000

    API for storing users added

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ async function run() {
         await client.connect();
         const database = client.db("melb-hack");
         const coursesCollection = database.collection("courses");
+        const usersCollection = database.collection("users");
 
         //GET COURSES FROM DB
         app.get('/courses', async (req, res) => {
@@ -39,6 +40,26 @@ async function run() {
             const query = { courseID: id }
             const course = await coursesCollection.findOne(query);
             res.json(course);
+        })
+
+        //Add users to database those who signed up with Email Password
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.json(result);
+        })
+
+        //Add users to database those who signed up with External Provider
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+
         })
 
 
